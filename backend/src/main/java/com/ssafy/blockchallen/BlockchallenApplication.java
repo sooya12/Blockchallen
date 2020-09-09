2c46a6d980bf441e4ae7cdacc6e9f706817eeed2
Park HyeoungMin <o_ogog@naver.com>
2020-09-09 10:19:59 +0900

    BE initial | 백엔드 기본 틀 구성 | S03P22A102-15

---
+++ backend/src/main/java/com/ssafy/blockchallen/BlockchallenApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.blockchallen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchallenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchallenApplication.class, args);
+    }
+
+}
