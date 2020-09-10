b256cf7bd8a3884367aa8ee8ac30a0e417ca9b28
최현수 <hyeonsu2014@naver.com>
2020-09-10 22:00:11 +0900

    [FE] update | block lottie 로고 및 카카오로그인 이미지 삽입 | S03P22A102-54

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,70 @@
 <template>
-  <div>
-    <p>Home</p>
+  <div id="app">
+    <v-content>
+      <div id="animationWindow">
+      </div>
+      <div id="title">
+        <p style="font-size:4vw;">
+          <b>BlockChallen</b>
+        </p>
+      </div>
+      <div id="kakaoLogin">
+        <img :src="require('@/assets/kakao_login_large_narrow.png')" @click="함수이름">
+      </div>
+    </v-content>
   </div>
 </template>
 
 <script>
-  export default {
-    name : 'Home'
+import lottie from 'lottie-web'
+
+export default {
+  name: 'Home',
+  mounted() {
+    this.anim = lottie.loadAnimation({
+      wrapper: document.querySelector('#animationWindow'),
+      animType: 'svg',
+      loop: true,
+      prerender: true,
+      autoplay: true,
+      path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+    })
+    this.$emit('animCreated', this.anim)
   }
+
+}
 </script>
 
-<style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Kalam:wght@700&display=swap');
+
+#app {
+  width: 70%;
+  height: auto;
+  margin: 0 auto;
+  font-family: 'Kalam', cursive;
+}
+
+#animationWindow {
+  width: auto;
+  height: 70%;
+}
+
+#title {
+  position: relative;
+  top: -13%;
+  text-align: center;
+}
+
+#kakaoLogin {
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+}
+
+#kakaoLogin img {
+  width: 30%;
+  height: auto;
+}
 
 </style>
