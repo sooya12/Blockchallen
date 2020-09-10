564c1042ef5d98f259913b326604272c29939773
Chaeyeong <lcy707@naver.com>
2020-09-10 19:02:38 +0900

    [FE] initial | 챌린지 리스트에서 마이페이지 이동 | S03P22A102-38

---
+++ frontend/src/views/challengeList.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="ChallengeList">
+        <h1>챌린지 리스트</h1>
+         <router-link to="/myPage">마이페이지로</router-link> 
+    </div>
+</template>
---
+++ frontend/src/views/myPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="MyPage">
+        <p>임시 마이페이지</p>
+    </div>
+</template>
