f3469e437b19bf28935a5bcc1b50787f901fc91f
1000min <o_ogog@naver.com>
2020-09-11 13:26:27 +0900

    [FE] initial | 입력 폼 만들기 |S03P22A102-36

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,11 @@ import router from './router'
 import store from './store'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 
 
 Vue.config.productionTip = false
-
 new Vue({
     router,
     store,
