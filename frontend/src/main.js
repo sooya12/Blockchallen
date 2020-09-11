f8c4a4992321f96c2e575711a2e1f8c5fe2b8722
1000min <o_ogog@naver.com>
2020-09-11 14:21:17 +0900

    [FE] initial | 생성 페이지 화면 설계 |S03P22A102-43

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,13 @@ import store from './store'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
 
-
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 new Vue({
     router,
