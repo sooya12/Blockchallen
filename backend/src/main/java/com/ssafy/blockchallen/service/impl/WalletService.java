666c999dee9c5153bc7a05a22ac5103d663b1669
JINSUB <neyo0311@naver.com>
2020-09-21 13:11:36 +0900

    [BE] update | 지갑 저장 재수정

--- backend/src/main/java/com/ssafy/blockchallen/service/impl/WalletService.java
+++ backend/src/main/java/com/ssafy/blockchallen/service/impl/WalletService.java
@@ -1,10 +1,13 @@
 package com.ssafy.blockchallen.service.impl;
 
 import com.ssafy.blockchallen.service.IWalletService;
+import com.ssafy.blockchallen.entity.Account;
 import com.ssafy.blockchallen.entity.Wallet;
+import com.ssafy.blockchallen.repository.AccountRepository;
 import com.ssafy.blockchallen.repository.WalletRepository;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,9 @@ public class WalletService implements IWalletService {
 
    @Autowired
    private WalletRepository walletRepository;
+   
+   @Autowired
+   private AccountRepository accountRepository;
 
    @Override
    public Wallet findById(long id) {
@@ -28,7 +34,8 @@ public class WalletService implements IWalletService {
    @Override
    public Wallet create(long id, String address) {
       Wallet newWallet = new Wallet();
-      newWallet.setId(id);
+      Optional<Account> account = accountRepository.findById(id);
+      newWallet.setAccount(account.get());
       newWallet.setAddress(address);
       return walletRepository.save(newWallet);
    }
