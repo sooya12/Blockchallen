43d4a149aec83c433ec5266b20659f7468a26a4d
Lee Yujin <ajph03102@naver.com>
2020-09-28 14:23:06 +0900

    [BE] update | controller 설명 추가

--- backend/src/main/java/com/ssafy/blockchallen/controller/ChallengeController.java
+++ backend/src/main/java/com/ssafy/blockchallen/controller/ChallengeController.java
@@ -23,6 +23,8 @@ import com.ssafy.blockchallen.dto.myChallengeDTO;
 import com.ssafy.blockchallen.dto.resultChallengeDTO;
 import com.ssafy.blockchallen.service.IChallengeService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/blockchallen")
@@ -31,6 +33,7 @@ public class ChallengeController {
 	@Autowired
 	private IChallengeService challengeService;
 	
+	@ApiOperation(value = "챌린지 생성")
 	@RequestMapping(path = "/challenge", method = RequestMethod.POST)	
 	public Object createChallenge(@RequestBody createChallengeDTO challenge) throws IOException {
 		
@@ -40,6 +43,7 @@ public class ChallengeController {
 		return new ResponseEntity<>("챌린지 생성 실패", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	@ApiOperation(value = "챌린지 상세 보기")
 	@RequestMapping(path = "/challenge", method = RequestMethod.GET)
 	public Object datailChallenge(@RequestParam long id) {
 		
@@ -49,12 +53,14 @@ public class ChallengeController {
 		else
 			return new ResponseEntity<>("존재하지 않는 챌린지", HttpStatus.NO_CONTENT);
 	}
-	
+
+	@ApiOperation(value = "챌린지 목록")
 	@RequestMapping(path = "/challenges", method = RequestMethod.GET)
 	public Object getChallenges(@RequestParam String option) {
 		return new ResponseEntity<>(challengeService.getChallenges(option),HttpStatus.OK);
 	}
 	
+	@ApiOperation(value = "나의 챌린지 목록")
 	@RequestMapping(path = "/mychallenges/{id}", method = RequestMethod.GET)
 	public Object myChallenges(@PathVariable("id") long id) throws ParseException {
 		
@@ -66,6 +72,7 @@ public class ChallengeController {
 			
 	}
 	
+	@ApiOperation(value = "챌린지 상세보기-인증목록")
 	@RequestMapping(path = "/challenge/certification", method = RequestMethod.GET)
 	public Object getCertifications(@RequestParam long id) {
 
@@ -76,6 +83,7 @@ public class ChallengeController {
 			return new ResponseEntity<>("존재하지 않는 챌린지", HttpStatus.NO_CONTENT);
 	}
 	
+	@ApiOperation(value = "챌린지 상세보기-결과")
 	@RequestMapping(path = "/challenge/result", method = RequestMethod.GET)
 	public Object challengeResult(@RequestParam long id) throws ParseException {
 		
@@ -86,6 +94,7 @@ public class ChallengeController {
 			return new ResponseEntity<>("존재하지 않는 챌린지", HttpStatus.NO_CONTENT);
 	}
 	
+	@ApiOperation(value = "챌린지 참가")
 	@RequestMapping(path = "/participate", method = RequestMethod.POST)
 	public Object challengeParticipate(@RequestBody idDTO ids) {
 		Object result = challengeService.challengeParticipate(ids); 
@@ -99,6 +108,7 @@ public class ChallengeController {
 			return new ResponseEntity<>("해당 회원 또는 챌린지를 찾을 수 없음", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
+	@ApiOperation(value = "챌린지 참가여부 확인")
 	@RequestMapping(path = "/participate", method = RequestMethod.GET)
 	public Object checkParticipate(@RequestParam long uid, @RequestParam long cid) {
 		Object result = challengeService.checkParticipate(uid, cid);
