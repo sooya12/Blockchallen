c146a222a09f4057349b286602fe3540e1b98e5a
Lee Yujin <ajph03102@naver.com>
2020-10-05 13:14:15 +0900

    [BE] update | Reward 엔티티 빌더 생성

--- backend/src/main/java/com/ssafy/blockchallen/entity/Reward.java
+++ backend/src/main/java/com/ssafy/blockchallen/entity/Reward.java
@@ -37,4 +37,34 @@ public class Reward {
 	
 	@ApiModelProperty(value = "상금")
 	private int prize;
+	
+	public static class Builder {
+		private Account account;
+		private Challenge challenge;
+		private int prize = 0;
+		
+		public Builder() {
+			
+		}
+		public Builder account(Account account) {
+			this.account = account;
+			return this;
+		}
+		public Builder challenge(Challenge challenge) {
+			this.challenge = challenge;
+			return this;
+		}
+		public Builder prize(int prize) {
+			this.prize = prize;
+			return this;
+		}
+		public Reward build() {
+			return new Reward(this);
+		}
+	}
+	private Reward(Builder builder) {
+		account = builder.account;
+		challenge = builder.challenge;
+		prize = builder.prize;
+	}
 }
